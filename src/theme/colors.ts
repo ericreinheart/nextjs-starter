58805f1617eb4a9430ff0f631881ca35f924450d
Jakob Werner <jakob.f.werner@icloud.com>
2024-02-26 12:36:08 -0500

    feat: added colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,6 +3,12 @@ import type { ThemeConfig } from 'tailwindcss/types/config'
 export const colors: ThemeConfig['colors'] = {
   transparent: 'transparent',
   current: 'currentColor',
-  black: '#10141D',
+  black: '#000000',
   white: '#FFFFFF',
+  orange: '#F7931A',
+  gray: {
+    500: '#818282',
+    700: '#252525',
+    900: '#1A191A',
+  },
 }
