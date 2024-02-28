3a7a0f7cf2bba4900a3eaedec9e5d9677b4e3fd7
Jakob Werner <jakob.f.werner@icloud.com>
2024-02-28 15:34:12 -0500

    feat: added ubuntu font

--- src/theme/fontFamily.ts
+++ src/theme/fontFamily.ts
@@ -2,12 +2,6 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 import type { ThemeConfig } from 'tailwindcss/types/config'
 
 export const fontFamily: ThemeConfig['fontFamily'] = {
-  title: [
-    // 'Inter',
-    ...defaultTheme.fontFamily.sans,
-  ],
-  sans: [
-    // 'Inter',
-    ...defaultTheme.fontFamily.sans,
-  ],
+  title: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+  sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
 }
--- src/theme/fontSize.ts
+++ src/theme/fontSize.ts
@@ -1,11 +1,86 @@
 import type { ThemeConfig } from 'tailwindcss/types/config'
 
 export const fontSize: ThemeConfig['fontSize'] = {
-  h1: [
-    '3.375rem', // 54px
+  // display
+  'display-2xl': [
+    '4.5rem', // 72px
     {
-      lineHeight: '4.25rem', // 68px
-      fontWeight: '700',
+      lineHeight: '1.25',
+      letterSpacing: '-0.02em',
+    },
+  ],
+
+  'display-xl': [
+    '3.75rem', // 60px
+    {
+      lineHeight: '4.5rem', // 72px
+      letterSpacing: '-0.02em',
+    },
+  ],
+
+  'display-lg': [
+    '3rem', // 48px
+    {
+      lineHeight: '1.25', // 60px
+      letterSpacing: '-0.02em',
+    },
+  ],
+
+  'display-md': [
+    '2.25rem', // 36px
+    {
+      lineHeight: '2.75rem', // 44px
+      letterSpacing: '-0.02em',
+    },
+  ],
+
+  'display-sm': [
+    '1.875rem', // 30px
+    {
+      lineHeight: '2.375rem', // 38px
+    },
+  ],
+
+  'display-xs': [
+    '1.5rem', // 24px
+    {
+      lineHeight: '2rem', // 32px
+    },
+  ],
+
+  //body
+  xl: [
+    '1.25rem', // 20px
+    {
+      lineHeight: '1.875rem', // 30px
+    },
+  ],
+
+  lg: [
+    '1.125rem', // 18px
+    {
+      lineHeight: '1.75rem', // 28px
+    },
+  ],
+
+  md: [
+    '1rem', // 16px
+    {
+      lineHeight: '1.5rem', // 24px
+    },
+  ],
+
+  sm: [
+    '0.875rem', // 14px
+    {
+      lineHeight: '1.25rem', // 20px
+    },
+  ],
+
+  xs: [
+    '0.75rem', // 12px
+    {
+      lineHeight: '1.125rem', // 18px
     },
   ],
 }
